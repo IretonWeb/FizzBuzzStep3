66c92c29f8b6e5f080d65d8fab70c0a7a015932e
IretonWeb <info@iretonweb.co.uk>
2013-11-12 12:41:13 +0000

    Fizz Buzz Step 3

---
+++ FizzBuzz.Tests/FBTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FizzBuzz;
+
+namespace FizzBuzz.Tests
+{
+    [TestFixture]
+    class FBTests
+    {
+
+
+        [Test]
+        public void MulitpleNumber3Test()
+        {
+            NUnit.Framework.StringAssert.AreEqualIgnoringCase("fizz", FizzBuzz.WorkOut(6));
+        }
+        [Test]
+        public void Number3Test()
+        {
+            NUnit.Framework.StringAssert.AreEqualIgnoringCase("lucky", FizzBuzz.WorkOut(13));
+        }
+        [Test]
+        public void Number5Test()
+        {
+
+            NUnit.Framework.StringAssert.AreEqualIgnoringCase("buzz", FizzBuzz.WorkOut(5));
+        }
+
+        [Test]
+        public void Number15Test()
+        {
+
+            NUnit.Framework.StringAssert.AreEqualIgnoringCase("fizzbuzz", FizzBuzz.WorkOut(15));
+        }
+
+        [Test]
+        public void DifferentNumberTest()
+        {
+            NUnit.Framework.StringAssert.AreEqualIgnoringCase("1", FizzBuzz.WorkOut(1));
+            
+        }
+
+        [Test]
+        public void ReportIntegerTest()
+        {
+           FizzBuzz.Display();
+            NUnit.Framework.Assert.AreEqual(10, FizzBuzz.IntCount);
+
+        }
+
+    }
+
+    
+}
---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+   public class FizzBuzz
+    {
+
+        private static int startVal = 1;
+        private static int endVal = 20;
+        private static int _intCount = 0;
+        private static int _buzzCount = 0;
+        private static int _fizzCount = 0;
+        private static int _luckyCount = 0;
+        private static int _fizzbuzzCount = 0;
+
+        public static int IntCount
+        {
+            get { return _intCount; }
+            set { _intCount = value; }
+        }
+
+
+
+        public static void Main()
+        {
+
+            Console.WriteLine("{0}", Display());
+        }
+
+        public static string Display()
+        {
+            StringBuilder output = new StringBuilder();
+            for (int i = startVal; i <= endVal; i++)
+            {
+                output.Append(WorkOut(i) + ' ');
+            }
+
+            output.AppendLine("fizz:" + _fizzCount);
+            output.AppendLine("buzz:" + _buzzCount);
+            output.AppendLine("fizzbuzz:" + _fizzbuzzCount);
+            output.AppendLine("lucky:" + _luckyCount);
+            output.AppendLine("Integer:" + _intCount);
+            return output.ToString();
+        }
+
+        public static string WorkOut(int number)
+        {
+            bool number3 = ((number % 3) == 0);
+            bool number5 = ((number % 5) == 0);
+      
+            if (number.ToString().Contains("3"))
+            {
+                _luckyCount += 1;
+                return "lucky";
+
+            }
+            if (number3 && number5)
+            {
+                _fizzbuzzCount += 1;
+                return "fizzbuzz";
+            }
+            else if(number3)
+            {
+                _fizzCount += 1;
+                return "fizz";
+            }
+            else if (number5)
+            {
+                _buzzCount += 1;
+                return "buzz";
+
+            }
+           
+            else
+            {
+                _intCount += 1;
+                return number.ToString();
+            }
+            
+        }
+
+        public static int ReportInteger()
+        {
+            return _intCount;
+        }
+    }
+}
